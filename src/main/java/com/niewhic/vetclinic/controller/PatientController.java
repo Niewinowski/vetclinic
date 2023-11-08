94d7c9f891e2cacac15647d3d371582fdb1f7663
Jan Hickiewicz <janhi@tutanota.com>
2023-11-08 11:54:46 +0100

    Patient model, controller and repository

--- src/main/java/com/niewhic/vetclinic/controller/PatientController.java
+++ src/main/java/com/niewhic/vetclinic/controller/PatientController.java
@@ -1,4 +1,43 @@
 package com.niewhic.vetclinic.controller;
 
+import com.niewhic.vetclinic.model.patient.Patient;
+import com.niewhic.vetclinic.repository.PatientRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/patient")
 public class PatientController {
+    private PatientRepository patientRepository = new PatientRepository();
+    // mappingi analogicznie jak w testcontrollerze
+    @GetMapping
+    public Collection<Patient> getAllPatients() {
+        return patientRepository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Patient getPatientById(@PathVariable long id) {
+        return patientRepository.getById(id);
+    }
+
+    @PostMapping
+    public void addPatient(@RequestBody Patient patient) {
+        patientRepository.add(patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable long id) {
+        patientRepository.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updatePatient(@PathVariable long id, @RequestBody Patient patient) {
+        patientRepository.update(id, patient);
+    }
+
+    @PatchMapping("/{id}")
+    public void editPatient(@PathVariable long id, @RequestBody Patient updatedPatient) {
+        patientRepository.edit(id, updatedPatient);
+    }
 }
--- src/main/java/com/niewhic/vetclinic/model/patient/Patient.java
+++ src/main/java/com/niewhic/vetclinic/model/patient/Patient.java
@@ -1,4 +1,92 @@
 package com.niewhic.vetclinic.model.patient;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public class Patient {
+    private long id;
+    private String name;
+    private String ownerName;
+    private String ownerLastName;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateOfBirth;
+    private String ownerEmail;
+    private String species;
+    private String breed;
+
+    public Patient(long id, String name, String ownerName, String ownerLastName, LocalDateTime dateOfBirth, String ownerEmail, String species, String breed) {
+        this.id = id;
+        this.name = name;
+        this.ownerName = ownerName;
+        this.ownerLastName = ownerLastName;
+        this.dateOfBirth = dateOfBirth;
+        this.ownerEmail = ownerEmail;
+        this.species = species;
+        this.breed = breed;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerLastName() {
+        return ownerLastName;
+    }
+
+    public void setOwnerLastName(String ownerLastName) {
+        this.ownerLastName = ownerLastName;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
